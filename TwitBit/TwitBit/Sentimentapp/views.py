f523dea407f1c818135a7a3d183e067e8e6bb418
Nick Guevara <nkg5096@gmail.com>
2018-03-13 18:09:47 -0400

    Added Bloomberg html

--- TwitBit/TwitBit/Sentimentapp/views.py
+++ TwitBit/TwitBit/Sentimentapp/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>Views page</h2>")
+    return render(request, 'personal/home.html')
 
